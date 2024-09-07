0812ea01cd0a6b08bad1a53a87fdac9dc868844b
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-09-07 12:28:27 +0200

    add extracting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from re import sub, IGNORECASE
-from os import rename, path
+from os import rename, path, remove
 from sys import exit
 from zipfile import ZipFile
 
@@ -12,22 +11,21 @@ def main() -> None:
         exit("Wrong file type entered.")
 
     # change to zip
-    extract_dir: str = ""
     zip_dir: str = base_dir + ".zip"
     rename(input_dir, zip_dir)
 
     # extract zip
     # create path to extracting files
     splits_dir = base_dir.split("\\")
-    for i in range(len(splits_dir)-1):
-        extract_dir += splits_dir[i]
+    extract_dir = path.join("\\".join(splits_dir[:-1]), "extracted")
     # extract into path
     with ZipFile(zip_dir, "r") as f:
         f.extractall(path=extract_dir)
-    # create folder name of extracted files
-    f_name: str = sub(r"^[a-z]:", "", extract_dir, flags=IGNORECASE)
-    f_name = sub(r"\\", "", f_name, flags=IGNORECASE)
-    sheets_dir: str = extract_dir + "\\" + f_name + "\\xl\\worksheets"
+
+    # delete old zip file
+    # remove(extract_dir[:-9])
+
+    sheets_dir: str = extract_dir + "\\xl\\worksheets"
     print(f"The Excel sheets are located at: {sheets_dir}")
 
 
