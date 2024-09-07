c5d148bfd479ba8aa066ec56d44e73348010f683
Kevin <149601399+gezxgen@users.noreply.github.com>
2024-09-07 08:02:03 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from re import search
+
+
+def main() -> None:
+    directory: str = input("Enter the URL of the Excel file (...\\my_excel.xlsx): ")
+    print(directory)
+
+
+if __name__ == "__main__":
+    main()
